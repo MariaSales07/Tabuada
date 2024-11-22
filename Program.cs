d96be714b046c3558f89a2e3b7352ba70c332fe7
sales <mariaclaradeasales@gmail.com>
2024-11-22 20:39:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int numero = 0;
+int segundo = 0;
+
+Console.WriteLine("Digite o numero: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+while(segundo <= 10){
+    Console.WriteLine($"{segundo} X {numero} = {segundo * numero}");
+    segundo++;
+}
+
